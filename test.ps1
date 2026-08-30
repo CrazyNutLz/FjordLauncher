fd2aa7a4238b46111c923a4682b7ad4e4184c938
CrazyNut <CrazyNutlz@outlook.com>
2026-08-30 14:44:54 +0800

    界面修改完成v2

--- test.ps1
+++ test.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = "Stop"
 
 . .\dev-env.ps1
 
+cmake -E touch .\launcher\ui\dialogs\AboutDialog.cpp
 cmake --build --preset windows_msvc --config Debug --target FjordLauncher
 
 if ($LASTEXITCODE -ne 0) {
